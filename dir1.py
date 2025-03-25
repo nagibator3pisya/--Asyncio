ff985a19fab99bcb0496d593857e03d3bd41b188
Станислав <morevstas02@mail.ru>
2025-03-25 14:31:18 +0300

    Task Задачи

--- dir1.py
+++ dir1.py
@@ -1,18 +1,80 @@
-# В каких задачах полезно асинхронное программирование
-# Ожидаемые объекты Awaitable
 import asyncio
 
-# функция корутина
-# async def exemple_coroutine():
-#     await asyncio.sleep(1)
-#     print('Привет')
 
-
-# async def exemple_coroutine(num,*args,**kwargs):
+# async def ex_cor():
 #     await asyncio.sleep(1)
-#     print(f"Hello from coroutine {num}!")
-#     return f"Корутина {num} завершила работу"
+#     print('Корутина')
 #
 #
-# asyncio.run(exemple_coroutine(5))
+# async def main():
+#     for i in range(10):
+#         await ex_cor() # запускается и ждем выполнения
+#
+# asyncio.run(main())# точка входа
 
+
+# async def cor_1():
+#     print('coro_1 says')
+#     await asyncio.sleep(2)
+#     print('hello coro_2')
+#
+#
+# async def cor_2():
+#     print('coro_2 says')
+#     await asyncio.sleep(2)
+#     print('hello coro_1')
+#
+#
+# async def main():
+#     await cor_1()
+#     await cor_2()
+#
+#
+# asyncio.run(main())
+
+
+
+# async def generate(n):
+#     await asyncio.sleep(0.1)
+#     print(f"Корутина generate с аргументом {n}")
+#
+#
+# async def main():
+#     for i in range(0,10):
+#         await generate(i)
+#
+# asyncio.run(main())
+
+async def coro_1():
+    print("Вызываю корутину 0")
+
+
+async def coro_5():
+    print("Вызываю корутину 3")
+    await coro_3()
+
+
+async def coro_3():
+    print("Вызываю корутину 2")
+    await coro_2()
+
+
+async def coro_4():
+    print("Вызываю корутину 1")
+    await coro_1()
+
+
+async def coro_2():
+    print("Вызываю корутину 4")
+    await coro_4()
+'''
+Вызываю корутину 3
+Вызываю корутину 2
+Вызываю корутину 4
+Вызываю корутину 1
+Вызываю корутину 0
+'''
+async def main():
+    await coro_5()
+
+asyncio.run(main())
