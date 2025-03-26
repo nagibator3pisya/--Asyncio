1f697f6575d9455b685fcb86b2c2062691df81ee
Станислав <morevstas02@mail.ru>
2025-03-26 22:09:57 +0300

    Решение задач

--- dir1.py
+++ dir1.py
@@ -1,80 +1,97 @@
 import asyncio
+"""#  asyncio.create_task"""
 
-
-# async def ex_cor():
+# async def example_coroutine():
 #     await asyncio.sleep(1)
-#     print('Корутина')
+#     print("Hello from coroutine!")
+#
+# async def main():
+#     task = asyncio.create_task(example_coroutine())
+#     await task
+#     print('Задачи finih')
+#
+# asyncio.run(main())
+
+"""asyncio.gether"""
+
+# async def example_coroutine():
+#     await asyncio.sleep(5)
+#     print("Hello from coroutine!")
 #
 #
 # async def main():
-#     for i in range(10):
-#         await ex_cor() # запускается и ждем выполнения
-#
-# asyncio.run(main())# точка входа
-
-
-# async def cor_1():
-#     print('coro_1 says')
-#     await asyncio.sleep(2)
-#     print('hello coro_2')
-#
-#
-# async def cor_2():
-#     print('coro_2 says')
-#     await asyncio.sleep(2)
-#     print('hello coro_1')
-#
-#
-# async def main():
-#     await cor_1()
-#     await cor_2()
-#
+#     tasks = []
+#     for _ in range(10):
+#         task = asyncio.create_task(example_coroutine()) # 10 задач создаем
+#         tasks.append(task)
+#     await asyncio.gather(*tasks) #  запускаем все задачи из списка tasks
 #
 # asyncio.run(main())
 
 
-
-# async def generate(n):
-#     await asyncio.sleep(0.1)
-#     print(f"Корутина generate с аргументом {n}")
+# async def log_data(data):
+#     await asyncio.sleep(1)
+#     print(f"Logged: {data}")
 #
 #
 # async def main():
-#     for i in range(0,10):
-#         await generate(i)
+#     data_to_log = ["Data 1", "Data 2", "Data 3"]
+#
+#     # задачи для логирования данных в фоновом режиме
+#     tasks = [asyncio.create_task(log_data(data)) for data in data_to_log]
+#     print('Продолжение выполнения других задач')
+#     await asyncio.gather(*tasks)
 #
 # asyncio.run(main())
 
-async def coro_1():
-    print("Вызываю корутину 0")
+
+# список поваров
+# chef_list = ['','Франсуа', 'Жан', 'Марсель']
+#
+#
+# async def cook_order(order_number: int, dish: str) -> list[tuple[int, str]]:
+#     """
+#     Фуннкция для приготовления заказов
+#     :param order_number: - номер заказа
+#     :param dish: - блюдо
+#     :return:
+#     """
+#     print(f'Повар {chef_list[order_number]} начинает готовить заказ №{order_number}: {dish}')
+#     await asyncio.sleep(2) # имитация готовки, загрузка данных из бз
+#     print(f'Заказ № {order_number}: {dish} готов')
+#     return f'{dish} для заказа № {order_number}'
+#
+#
+# async def serve_order(order_number: int, dish: str) -> list[tuple[int, str]]:
+#     """
+#
+#     :param order_number: - номер заказа
+#     :param dish: - блюдо
+#     """
+#     await cook_order(order_number,dish) # подождать пока оно приготовиться
+#     print(f'Официант подает {dish}')
+#
+#
+# async def manager():
+#     '''
+#     Менеджер событный цикл назначение задачи
+#     :return:
+#     '''
+#     orders = [(1,'салат'),(2,'стейк'),(3,'суп')]
+#     tasks = [asyncio.create_task(serve_order(order_number,dish)) for order_number,dish in orders]
+#     await asyncio.gather(*tasks)
+#
+# asyncio.run(manager())
 
 
-async def coro_5():
-    print("Вызываю корутину 3")
-    await coro_3()
 
 
-async def coro_3():
-    print("Вызываю корутину 2")
-    await coro_2()
 
 
-async def coro_4():
-    print("Вызываю корутину 1")
-    await coro_1()
 
 
-async def coro_2():
-    print("Вызываю корутину 4")
-    await coro_4()
-'''
-Вызываю корутину 3
-Вызываю корутину 2
-Вызываю корутину 4
-Вызываю корутину 1
-Вызываю корутину 0
-'''
-async def main():
-    await coro_5()
 
-asyncio.run(main())
+
+
+
+
