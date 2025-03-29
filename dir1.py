e1de01637f69958c6981db14960df4a7736ff607
Станислав <morevstas02@mail.ru>
2025-03-29 23:42:44 +0300

    #Счётчики #2, реализация дальше по ветке

--- dir1.py
+++ dir1.py
@@ -1,23 +1,36 @@
 import asyncio
-from itertools import count
+
 
 # Счётчики #1
 
 # Counter 1 - имя счётчика
 # 13 - максимальное значение для счётчика Counter_1
 
+# max_counts = {
+#     "Counter 1": 13,
+#     "Counter 2": 7
+# }
 max_counts = {
-    "Counter 1": 13,
-    "Counter 2": 7
+    "Counter 1": 10,
+    "Counter 2": 5,
+    "Counter 3": 15
+}
+
+delays = { # нужно было добавить счётчик при вызове  delays['Counter 1']
+    "Counter 1": 1,
+    "Counter 2": 2,
+    "Counter 3": 0.5
+}
+counters = {
+    "Counter 1": 0,
+    "Counter 2": 0,
+    'Counter 3': 0
 }
 """
 Создайте словарь counters, где ключи — это имена счётчиков ("Counter 1" и "Counter 2"), а
  значения —  это текущие значения счётчиков, изначально равные нулю.
 """
-counters = {
-    "Counter 1": 0,
-    "Counter 2": 0
-}
+
 '''
 Напишите асинхронную функцию counter, которая принимает имя счётчика и задержку. 
 В цикле эта функция должна увеличивать значение соответствующего 
@@ -29,6 +42,7 @@ counters = {
 
 
 async def counter(name: str, delay: int)-> None:
+
     while counters[name] < max_counts[name]:
         counters[name] += 1 # увеличение
         await asyncio.sleep(delay)
@@ -37,10 +51,13 @@ async def counter(name: str, delay: int)-> None:
 
 
 async def main():
-    task1 = asyncio.create_task(counter('Counter 1',1))
-    task2 = asyncio.create_task(counter('Counter 2', 1))
+    #  gether() придумать
+    task1 = asyncio.create_task(counter('Counter 1', delays['Counter 1']))
+    task2 = asyncio.create_task(counter('Counter 2', delays['Counter 2']))
+    task3 = asyncio.create_task(counter('Counter 3', delays['Counter 3']))
     await task1
     await task2
+    await task3
 asyncio.run(main())
 
 
