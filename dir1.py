6bb8b78ae819fba7a4d3bb9b79eba159b8a9b64d
Станислав <morevstas02@mail.ru>
2025-03-31 21:17:51 +0300

    Установка результата для объектов Future. Метод set_result()

--- dir1.py
+++ dir1.py
@@ -1,73 +1,43 @@
 import asyncio
 
-
-# Счётчики #1
-
-# Counter 1 - имя счётчика
-# 13 - максимальное значение для счётчика Counter_1
-
-# max_counts = {
-#     "Counter 1": 13,
-#     "Counter 2": 7
-# }
-max_counts = {
-    "Counter 1": 10,
-    "Counter 2": 5,
-    "Counter 3": 15
-}
-#  task1 = asyncio.create_task(counter('Counter 1', delays['Counter 1']))
-delays = { # нужно было добавить счётчик при вызове  delays['Counter 1']
-    "Counter 1": 1,
-    "Counter 2": 2,
-    "Counter 3": 0.5
-}
-counters = {
-    "Counter 1": 0,
-    "Counter 2": 0,
-    'Counter 3': 0
-}
-"""
-Создайте словарь counters, где ключи — это имена счётчиков ("Counter 1" и "Counter 2"), а
- значения —  это текущие значения счётчиков, изначально равные нулю.
-"""
-
-'''
-Напишите асинхронную функцию counter, которая принимает имя счётчика и задержку. 
-В цикле эта функция должна увеличивать значение соответствующего 
-счётчика в словаре counters на 1,  делать паузу на одну секунду и  
-выводить сообщение с именем счетчика и его текущим значением. 
-Этот цикл должен продолжаться до тех пор, 
-пока значение счётчика не достигнет соответствующего значения в словаре max_counts.
-'''
+# Создание обьекта future
+future = asyncio.Future()
 
 
-async def counter(name: str)-> None:
+# async def exmple_task():
+#     return 'Задача выполнена'
+#
+#
+#
+# async  def main():
+#     task = asyncio.create_task(exmple_task()) # создание задачи из корутины example_task()
+#     await task  # запуск задачи и ожидание выполнения
+#     result = task.result()  # сохранение результата в переменную result
+#     print(result)
+#
+#
+# asyncio.run(main())
 
-    while counters[name] < max_counts[name]:
-        counters[name] += 1 # увеличение
-        await asyncio.sleep(delays[name])
-        print(f"{name}: {counters[name]}")
+# Установка результата для объектов Future. Метод set_result()
 
+
+async  def simulate_long_running_task(name, dalay, future: asyncio.Future):
+    print(f'Задача {name} началась, будет выполнена за {dalay} сек')
+    await asyncio.sleep(dalay)
+    result = f'Результат задачи {name}'
+    print(f'Задача {name}, завершина')
+    future.set_result(result) # УСТАНАВЛИВАЕМ результат для Future объекта
 
 
 async def main():
-    #  gether() придумать
-    # task1 = asyncio.create_task(counter('Counter 1', ))
-    # task2 = asyncio.create_task(counter('Counter 2', ))
-    # task3 = asyncio.create_task(counter('Counter 3',))
-    # await task1
-    # await task2
-    # await task3
-    task = [asyncio.create_task(counter(name)) for name in counters]
-    await asyncio.gather(*task)
+    # объект футуры
+    future = asyncio.Future()
+    # запускаем корутину , передаем future объект в функцию
+    await simulate_long_running_task('Задача 1',3,future)
+    result = future.result()
+    print(f'Результат Future: {result}')
+
 asyncio.run(main())
-
-
-
-
-
-
-
 
 
 
