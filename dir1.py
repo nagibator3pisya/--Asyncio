44d91d62613973a93ada874a0808fa3f6988f83f
Станислав <morevstas02@mail.ru>
2025-04-05 18:31:56 +0300

    Решение задачи Методы состояния Task и Future.

--- dir1.py
+++ dir1.py
@@ -1,53 +1,34 @@
 import asyncio
-# Методы done(),cancel(),cancelled()
-# Pending (ожидание)
-
-async def main():
-    future = asyncio.Future()
-    if not future.done():
-        print('Состояние: Pending')
-    try:
-        result = future.result()
-        print(result)
-    except asyncio.InvalidStateError:
-        print('Задача ещё не выполнена. Доступа к результатам нет!')
 
 
-async def main_Completed():
-    future = asyncio.Future()
-    future.set_result('Задача завершена')
-    result = future.result()
-
-    if future.done():
-        print('Состояние : Completed(завершено)')
-        print(f'Результат: {result} ')
-
-
-async  def main_Cancelled():
-    future = asyncio.Future()
-    future.cancel()
-    if future.cancelled():
-        print('Состояние Cancelled (Отменено)')
-    try:
-        result =future.result()
-        print(result)
-    except asyncio.CancelledError:
-        print('Задача отменена. Доступа к результатам нет')
-
-# if __name__ == '__main__':
-#     # asyncio.run(main())
-#     # asyncio.run(main_Completed())
-#     asyncio.run(main_Cancelled())
-
-
-async def set_after(fut, delay, value):
+async def set_future_result(value,delay):
+    print(f"Задача начата. Установка результата '{value}' через {delay} секунд.")
     await asyncio.sleep(delay)
-    fut.set_result(value)
+    print("Результат установлен.")
+    return value
+
+
+async def create_and_use_future():
+    task = asyncio.create_task(set_future_result('Успех',2))
+    if not task.done():
+        print("Состояние Task до выполнения: Ожидание")
+    else:
+        print("Состояние Task до выполнения: Завершено")
+
+    print("Задача запущена, ожидаем завершения...")
+    await task
+
+    if task.done():
+        print("Состояние Task после выполнения: Завершено")
+    else:
+        print("Состояние Task после выполнения: Ожидание")
+    return task.result()
+
 
 async def main():
-    future = asyncio.Future()
-    asyncio.ensure_future(set_after(future, 1, 'done'))
-    result = await future
-    print(result)
 
-asyncio.run(main())
+    result = await create_and_use_future()
+    print("Результат из Task:", result)
+
+if __name__ == '__main__':
+    asyncio.run(main())
