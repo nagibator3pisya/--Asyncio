c36e17fb70f0aeddc48ccd7e577165f4fdfe49dd
user <morevstas02@mail.ru>
2025-04-09 12:21:13 +0300

    выполнения задачи: Читаем книгу асинхронно ' ЭТО ЗАДАЧА'

--- dir1.py
+++ dir1.py
@@ -1,44 +1,18 @@
 import asyncio
-import time
-# использование await с корутиной
-
-async def coro(num,second):
-    print(f'coro {num}, начало свое выполнение')
-    await asyncio.sleep(second)
-    print(f'coro {num} выполнена за {second} секунд(ы)')
+async def read_book(student, time):
+    print(f"{student} начал читать книгу.")
+    await asyncio.sleep(time)
+    print(f"{student} закончил читать книгу за {time} секунд.")
 
 
 async def main():
-    # создание объектов корутин путем вызова корутинной функции
-    coro1 = coro(1,2)
-    coro2 = coro(2,1)
-    # запуск и ожидание выполнения объектов корутин
-    await coro1
-    await coro2
-
-start = time.time()
-asyncio.run(main())
-
-print(f'ПРОГРАММА ВЫПОЛНЕНА ЗА {time.time() - start:.3f}')
-
-# Использование корутин с использованием Task
-
-async def coro(num,second):
-    print(f'coro {num}, начало свое выполнение')
-    await asyncio.sleep(second)
-    print(f'coro {num} выполнена за {second} секунд(ы)')
-
-
-async def main():
-    # создание объектов корутин путем вызова корутинной функции
-    task1 = asyncio.create_task(coro(1,2))
-    task2 = asyncio.create_task(coro(2, 1))
-    # запуск и ожидание выполнения объектов корутин
+    task1 = asyncio.create_task(read_book('Алекс',5))
+    task2 = asyncio.create_task(read_book('Мария', 3))
+    task3 = asyncio.create_task(read_book('Иван', 4))
     await task1
     await task2
+    await task3
 
 
-start = time.time()
-asyncio.run(main())
-
-print(f'ПРОГРАММА ВЫПОЛНЕНА ЗА {time.time() - start:.3f}')
+if __name__ == '__main__':
+    asyncio.run(main())
