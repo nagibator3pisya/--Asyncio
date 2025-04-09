b01892c2ae438ce449c615b9881a26bcfd054cdb
user <morevstas02@mail.ru>
2025-04-09 13:21:33 +0300

    выполнения задачи: Марафон знаний

--- dir1.py
+++ dir1.py
@@ -1,18 +1,26 @@
 import asyncio
-async def read_book(student, time):
-    print(f"{student} начал читать книгу.")
-    await asyncio.sleep(time)
-    print(f"{student} закончил читать книгу за {time} секунд.")
+
+students = {
+    "Алекс": {"course": "Асинхронный Python", "steps": 515, "speed": 78},
+    "Мария": {"course": "Многопоточный Python", "steps": 431, "speed": 62},
+    "Иван": {"course": "WEB Парсинг на Python", "steps": 491, "speed": 57}
+}
+
+async def study_course(student, course, steps, speed):
+    reading_time = round(steps / speed,2)
+    print(f'{student} начал проходить курс {course}.')
+    await asyncio.sleep(reading_time)
+    print(f"{student} прошел курс {course} за {reading_time} ч.")
+
 
 
 async def main():
-    task1 = asyncio.create_task(read_book('Алекс',5))
-    task2 = asyncio.create_task(read_book('Мария', 3))
-    task3 = asyncio.create_task(read_book('Иван', 4))
-    await task1
-    await task2
-    await task3
-
+    task = []
+    for student,deteil in students.items():
+        tt = asyncio.create_task(study_course(student,deteil['course'],deteil['steps'],deteil['speed']))
+        task.append(tt)
+    await asyncio.gather(*task)
 
 if __name__ == '__main__':
     asyncio.run(main())
+
