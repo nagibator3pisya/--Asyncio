95a98d2fd3c500e2eb658e3614b387e1731bdb66
user <morevstas02@mail.ru>
2025-04-10 13:05:50 +0300

    Ключевое слово await и # await блокирует выполнение корутины

--- dir1.py
+++ dir1.py
@@ -1,26 +1,25 @@
 import asyncio
+# Ключевое слово await
 
-students = {
-    "Алекс": {"course": "Асинхронный Python", "steps": 515, "speed": 78},
-    "Мария": {"course": "Многопоточный Python", "steps": 431, "speed": 62},
-    "Иван": {"course": "WEB Парсинг на Python", "steps": 491, "speed": 57}
-}
+# async def my_coro(num):
+#     print(f'Начинаем выполнение корутины {num}')
+#     await asyncio.sleep(1)
+#     print(f'Закончили выполнение корутины {num}')
+#
+# async  def main():
+#     task = [asyncio.create_task(my_coro(i)) for i in range(1,6)]
+#     await asyncio.gather(*task)
 
-async def study_course(student, course, steps, speed):
-    reading_time = round(steps / speed,2)
-    print(f'{student} начал проходить курс {course}.')
-    await asyncio.sleep(reading_time)
-    print(f"{student} прошел курс {course} за {reading_time} ч.")
+# await блокирует выполнение корутины
 
-
+async def coro():
+    print(f'Начинает выполнение')
+    await asyncio.sleep(1)  # Тут блокируется корутина сoro() до завершения asyncio.sleep().
+    print(f'Закончил')
 
 async def main():
-    task = []
-    for student,deteil in students.items():
-        tt = asyncio.create_task(study_course(student,deteil['course'],deteil['steps'],deteil['speed']))
-        task.append(tt)
-    await asyncio.gather(*task)
+    await coro() # Тут блокируется main() до завершения my_coroutine().
+    print('Выполнение корутины закончили')
 
 if __name__ == '__main__':
     asyncio.run(main())
-
