e6838aac13e80bc7cee339fb17065b82cbb8b2a1
user <morevstas02@mail.ru>
2025-04-18 15:25:19 +0300

    Задача 1

--- dir1.py
+++ dir1.py
@@ -1,21 +1,25 @@
 import asyncio
-# Задачи
 
-# Пример данных
-log_events = [
-    {"event": "Запрос на вход", "delay": 0.5},
-    {"event": "Запрос данных пользователя", "delay": 1.0},
-    {"event": "Обновление данных пользователя", "delay": 1.5},
-    {"event": "Обновление конфигурации сервера", "delay": 5.0}
-    ]
+async def coroutine_1():
+    print("Первое сообщение от корутины 1")
+    await asyncio.sleep(3)  # Подберите необходимую задержку
+    print("Второе сообщение от корутины 1")
 
-async def fetch_log(event):
-    delay = event.get('delay')
-    await asyncio.sleep(delay)
-    print(f"Событие: '{event['event']}' обработано с задержкой {delay} сек.")
+async def coroutine_2():
+    print("Первое сообщение от корутины 2")
+    await asyncio.sleep(2)  # Подберите необходимую задержку
+    print("Второе сообщение от корутины 2")
+
+async def coroutine_3():
+    print("Первое сообщение от корутины 3")
+    await asyncio.sleep(1)  # Подберите необходимую задержку
+    print("Второе сообщение от корутины 3")
 
 async def main():
-    task = [asyncio.create_task(fetch_log(i)) for i in log_events]
-    result = await asyncio.gather(*task)
+    await asyncio.gather(
+        coroutine_1(),
+        coroutine_2(),
+        coroutine_3(),
+    )
 
 asyncio.run(main())
